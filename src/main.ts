3c80d87afa4d6475209102e18c5c291aec877cd3
Rizaldi87 <rizaldiibnum@gmail.com>
2026-01-06 20:38:22 +0700

    initialize swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt.auth.guard';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,18 @@ async function bootstrap() {
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
   // app.useGlobalGuards(new JwtAuthGuard());
+
+  const config = new DocumentBuilder()
+    .setTitle('CRACK BE Docs')
+    .setDescription('CRACK PROJECT BE API')
+    .setVersion('1.0')
+    .addTag('CRACK')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, documentFactory());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
