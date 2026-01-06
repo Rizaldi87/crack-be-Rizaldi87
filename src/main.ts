1ea404a05dbcbf349b8c286c9e532aae9364890a
Rizaldi87 <rizaldiibnum@gmail.com>
2026-01-06 20:32:32 +0700

    enable cors, add global prefix and global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { JwtAuthGuard } from './auth/jwt.auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalGuards(new JwtAuthGuard());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
