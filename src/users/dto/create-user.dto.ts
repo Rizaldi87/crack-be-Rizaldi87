cbd79b3ddfd3cdf0f139861585d0745604d2f72f
Rizaldi87 <rizaldiibnum@gmail.com>
2026-01-07 12:45:39 +0700

    feat (create user dto) : add docs

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Role } from '@prisma/client';
 import {
   IsEmail,
@@ -7,18 +8,22 @@ import {
 } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ example: 'Rizaldi' })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({ example: 'admin@mail.com' })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: 'password123!' })
   @IsNotEmpty()
   @IsStrongPassword()
   password: string;
 
+  @ApiProperty({ example: 'admin' })
   @IsNotEmpty()
   role: Role;
 }
