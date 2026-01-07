282360c2e87ac1a0b5e40479df67eaaa5f02c5dc
Rizaldi87 <rizaldiibnum@gmail.com>
2026-01-07 12:43:49 +0700

    add modules : lessons module & quiz module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { PrismaModule } from './prisma/prisma.module';
 import { CoursesModule } from './courses/courses.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { LessonsModule } from './lessons/lessons.module';
+import { QuizModule } from './quiz/quiz.module';
 
 @Module({
-  imports: [PrismaModule, CoursesModule, AuthModule, UsersModule],
+  imports: [PrismaModule, CoursesModule, AuthModule, UsersModule, LessonsModule, QuizModule],
   controllers: [AppController],
   providers: [AppService],
 })
